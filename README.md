dfa6916d0ff79556abda57dffb9eb6c2b28a3396
unknown <peulsilva9@gmail.com>
2021-12-09 22:01:55 -0300

    feat: add a README file

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# giants steps summer
- 
+# Mario Kart Challenge
+
+O código em Python simula para os inputs fornecidos, as probabilidades do primeiro mestre do Mario Kart ser campeão.
+
+## Instalação
+
+Use o controlador de pacotes [pip](https://pip.pypa.io/en/stable/) para instalar as dependências.
+
+```bash
+pip install pandas
+
+
+pip install matplotlib
+```
+
+## Uso
+
+Dado o input campeonatos.txt, para executar o código, rode o seguinte comando:
+
+```bash
+python main.py
+```
+
+As figuras serão geradas na pasta ./figures
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- multiplePlayers.py
+++ multiplePlayers.py
@@ -7,31 +7,24 @@ def simulatedChampionshipWinner(players):
     numPlayers = len(players)
     challengers = players.copy()
     challengers.pop(0)
-    # print(players[master].winsInARow)
     while not championshipEnd:
     
         participant = challengers.pop(0)
-        # print(master, participant.index, challengers[0].index)
         if simulatedMatchWinner(participant, players[master]) == master:
             players[master].addWin()
             challengers.append(participant)
-            # print(challengers[0].index, challengers[1].index)
             if isChampion(players[master], numPlayers):
-                # print(master, "is champions with", players[master].winsInARow, len(players))
                 return master        
         else:
             if simulatedMatchWinner(participant, players[master]) == participant.index:
                 players[master].restartCounter()
                 challengers.append(players[master])
-                # print(challengers[0].index, challengers[1].index)
                 master = participant.index
 
             else:
                 players[master].addWin()
                 challengers.append(participant)
-                # print(challengers[0].index, challengers[1].index)
                 if isChampion(players[master], numPlayers):
-                    # print(master, "is champions with", players[master].winsInARow, len(players))
                     return master
     
 
